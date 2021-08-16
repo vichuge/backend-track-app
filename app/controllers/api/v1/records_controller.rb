ff2e35cc8aaf0cda8672fcf5bb39a25d514b83d3
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-08-16 16:59:26 -0500

    Add mv linters

--- app/controllers/api/v1/records_controller.rb
+++ app/controllers/api/v1/records_controller.rb
@@ -2,41 +2,48 @@ module Api
   module V1
     class RecordsController < ApplicationController
       before_action :set_record, only: %i[update show destroy]
-     # GET /records
-     def index
-       @records = Record.all
-       render json: RecordsRepresenter.new(@records).as_json
-     end
-     # POST /record
-     def create
-       @record = Record.create(record_params)
-       if @record.save
-         render json: RecordRepresenter.new(@record).as_json, status: :created
-       else
-         render json: @record.errors, status: :unprocessable_entity
-       end
-     end
-     # GET /records/:id
-     def show
-       render json: RecordRepresenter.new(@record).as_json
-     end
-     # PUT /records/:id
-     def update
-       @record.update(record_params)
-       head :no_content
-     end
-     # DELETE /records/:id
-     def destroy
-       @record.destroy
-       head :no_content
-     end
-     private
-     def record_params
-       params.permit(:times, :list_id)
-     end
-     def set_record
-       @record = Record.find(params[:id])
-     end
+      # GET /records
+      def index
+        @records = Record.all
+        render json: RecordsRepresenter.new(@records).as_json
+      end
+
+      # POST /record
+      def create
+        @record = Record.create(record_params)
+        if @record.save
+          render json: RecordRepresenter.new(@record).as_json, status: :created
+        else
+          render json: @record.errors, status: :unprocessable_entity
+        end
+      end
+
+      # GET /records/:id
+      def show
+        render json: RecordRepresenter.new(@record).as_json
+      end
+
+      # PUT /records/:id
+      def update
+        @record.update(record_params)
+        head :no_content
+      end
+
+      # DELETE /records/:id
+      def destroy
+        @record.destroy
+        head :no_content
+      end
+
+      private
+
+      def record_params
+        params.permit(:times, :list_id)
+      end
+
+      def set_record
+        @record = Record.find(params[:id])
+      end
     end
   end
 end
--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -8,6 +8,6 @@ RSpec.describe List, type: :model do
   it { should validate_presence_of(:icon) }
   it {
     should validate_length_of(:title)
-     .is_at_least(3)
+      .is_at_least(3)
   }
 end
