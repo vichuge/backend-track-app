5b2654cf9166756bb9204bb8b26869fd31f2acc9
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-08-31 16:14:42 -0500

    Delete patch nick from routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :lists, only: %i[index create destroy]
       resources :records, only: %i[index create show update destroy]
-      resources :authentication, only: [:create], path: 'login'
-      resources :users, only: [:create], path: 'register'
+      resources :authentication, only: [:create]
+      resources :users, only: [:create]
     end
   end
 end
