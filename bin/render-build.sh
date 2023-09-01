f9c5eb99abde576f3b1bc02c3d3da711e458d68c
vichuge <victor.hugo.pacheco.flores@gmail.com>
2023-09-01 13:11:07 -0600

    Add gems intall on dev and test env

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,7 @@
 # exit on error
 set -o errexit
 
-bundle install
+bundle install --with development test
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
