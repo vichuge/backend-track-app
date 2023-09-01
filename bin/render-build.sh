a815c2a0ff982fda3b46c363ec93f4e0fecce844
vichuge <victor.hugo.pacheco.flores@gmail.com>
2023-09-01 14:25:38 -0600

    restore changes

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,7 @@
 # exit on error
 set -o errexit
 
-bundle install --with development test
+bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
